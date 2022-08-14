002946ad9b6d4e72ae97a60377630e9aa83d64dd
lighttall <ta8729@gmail.com>
2022-08-14 12:52:03 +0300

    first commit

---
+++ src/components/List/styles.js
@@ -0,0 +1,22 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles((theme) => ({
+  container: {
+    padding: "25px",
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    marginBottom: "30px",
+  },
+  list: {
+    height: "75vh",
+    overflow: "auto",
+  },
+  loading: {
+    height: "600px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}));
